468c675540b68e593def92d1f9e9cb411e9e1df6
haruakinahimwise <haruakinahimwise@gmail.com>
2026-06-02 11:38:12 +0900

    Update servers.js

--- docs/js/servers.js
+++ docs/js/servers.js
@@ -31,3 +31,19 @@ async function selectServer(server) {
 }
 
 export { loadServers, selectServer };
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log("Server running"));
