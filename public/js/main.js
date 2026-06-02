431ae181c04404496b57d121ae2f12cd3e09d251
haruakinahimwise <haruakinahimwise@gmail.com>
2026-06-02 10:49:55 +0900

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,24 @@
+import { loadSession, clearSession } from "./state.js";
+import { initCloak } from "./cloak.js";
+import { loadServers } from "./servers.js";
+import { initChatInput } from "./chat.js";
+import { renderUserPanel } from "./profile.js";
 
+window.addEventListener("DOMContentLoaded", async () => {
+  loadSession();
+
+  if (!window.currentUser) {
+    window.location.href = "login.html";
+    return;
+  }
+
+  renderUserPanel();
+  initCloak();
+  initChatInput();
+  await loadServers();
+
+  document.getElementById("logout-btn").onclick = () => {
+    clearSession();
+    window.location.href = "login.html";
+  };
+});
