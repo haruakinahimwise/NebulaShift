e0d8507bf229e6646906a9b930c75e4658bfd983
haruakinahimwise <haruakinahimwise@gmail.com>
2026-06-02 10:48:57 +0900

    Update cloak.js

--- public/js/cloak.js
+++ public/js/cloak.js
@@ -1,1 +1,36 @@
+let isCloaked = true;
 
+function applyCloak() {
+  const cloak = document.getElementById("cloak-root");
+  const app = document.getElementById("app-root");
+
+  if (isCloaked) {
+    document.title = "Googl";
+    document.getElementById("favicon").href = "googl-favicon.png";
+    cloak.classList.remove("hidden");
+    app.classList.add("hidden");
+  } else {
+    document.title = "NebulaShift";
+    document.getElementById("favicon").href = "nebula-favicon.png";
+    cloak.classList.add("hidden");
+    app.classList.remove("hidden");
+  }
+}
+
+function initCloak() {
+  applyCloak();
+
+  window.addEventListener("keydown", e => {
+    if (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === "x") {
+      isCloaked = !isCloaked;
+      applyCloak();
+    }
+  });
+
+  window.addEventListener("blur", () => {
+    isCloaked = true;
+    applyCloak();
+  });
+}
+
+export { initCloak };
